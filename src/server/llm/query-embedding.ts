863f4a442abbd0f11fe7b9ef2f5cad2e266e79f6
Adib Saad <adib.saad@gmail.com>
2026-03-10 23:34:29 -0400

    Refactor query methods

--- src/server/llm/query-embedding.ts
+++ src/server/llm/query-embedding.ts
@@ -1,9 +1,10 @@
-import { cosineDistance, desc, gt, eq, and, sql } from 'drizzle-orm'
+import { cosineDistance, desc, eq, and, sql, gte } from 'drizzle-orm'
 
 import { db } from '@server/drizzle/db'
 import { fileUpload, fileUploadChunk, memories } from '@server/drizzle/schema'
 import { generateEmbedding } from '@server/llm/embed'
 
+const MEMORY_MIN_SIMILARITY = 0.5
 export const findRelatedMemoriesForUser = async (
   userId: number,
   query: string,
@@ -17,11 +18,14 @@ export const findRelatedMemoriesForUser = async (
   return db
     .select({ name: memories.content, similarity })
     .from(memories)
-    .where(and(gt(similarity, 0.5), eq(memories.userId, userId)))
+    .where(
+      and(gte(similarity, MEMORY_MIN_SIMILARITY), eq(memories.userId, userId)),
+    )
     .orderBy(t => desc(t.similarity))
     .limit(4)
 }
 
+const FILE_MIN_SIMILARITY = 0.5
 export const findRelatedFileChunksForUser = async (
   userId: number,
   query: string,
@@ -36,7 +40,9 @@ export const findRelatedFileChunksForUser = async (
     .select({ name: fileUploadChunk.content, similarity })
     .from(fileUploadChunk)
     .leftJoin(fileUpload, eq(fileUploadChunk.fileUploadId, fileUpload.id))
-    .where(and(gt(similarity, 0.5), eq(fileUpload.userId, userId)))
+    .where(
+      and(gte(similarity, FILE_MIN_SIMILARITY), eq(fileUpload.userId, userId)),
+    )
     .orderBy(t => desc(t.similarity))
     .limit(4)
 }
